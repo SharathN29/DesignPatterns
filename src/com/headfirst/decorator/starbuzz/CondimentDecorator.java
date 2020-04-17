9ef8e8d6eaa89b838af78271534e25ec08263454
sharathn29 <nsharathn@gmail.com>
2020-04-16 21:28:19 -0400

    Decorator Pattern
    Simple coffe shop example.

---
+++ src/com/headfirst/decorator/starbuzz/CondimentDecorator.java
@@ -0,0 +1,7 @@
+package com.headfirst.decorator.starbuzz;
+
+public abstract class CondimentDecorator extends Beverage{
+	Beverage beverage; 
+	
+	public abstract String getDescription();
+}
---
+++ src/com/headfirst/decorator/starbuzz/Soy.java
@@ -0,0 +1,16 @@
+package com.headfirst.decorator.starbuzz;
+
+public class Soy extends CondimentDecorator {
+
+	public Soy(Beverage beverage) {
+		this.beverage = beverage;
+	}
+	
+	public String getDescription() {
+		return beverage.getDescription() + ", Soy";
+	}
+
+	public double cost() {
+		return beverage.cost() + 0.15;
+	}
+}
